7b0cc54c5ba76dfbe28f7cd023e4ea5cbd718d5d
Matt Basta <me@mattbasta.com>
2012-08-10 14:45:04 -0700

    Improved tests for ZipManager

--- tests/test_xpimanager.py
+++ tests/test_xpimanager.py
@@ -1,9 +1,10 @@
 # -*- coding: utf8 -*-
 import os
 import tempfile
-from zipfile import BadZipfile, ZipFile
 
-from nose.tools import eq_
+from nose.tools import eq_, raises
+
+from helper import TestCase
 
 from appvalidator.zip import ZipPackage
 
@@ -14,53 +15,72 @@ def get_path(fn):
     return os.path.join(RESOURCES_PATH, fn)
 
 
-def test_open():
-    """Test that the manager will open the package."""
-    z = ZipPackage(get_path('xpi/install_rdf_only.xpi'))
-    assert z is not None
+class TestZipManager(TestCase):
+    def setUp(self):
+        self.z = ZipPackage(get_path('xpi/install_rdf_only.xpi'))
+        super(TestZipManager, self).setUp()
+
+    def test_open(self):
+        """Test that the manager will open the package."""
+        assert self.z is not None
+
+    def test_get_list(self):
+        """Test that the manager can read the file listing."""
+        assert not self.z.contents_cache
+        assert self.z.package_contents()
+        assert self.z.contents_cache  # Spelling check!
+        self.z.contents_cache = 'foo'
+        eq_(self.z.package_contents(), 'foo')
+
+    def test_get_list_broken_fail(self):
+        """
+        Test that the manager will generate a new package listing when broken
+        files have been detecetd.
+        """
+        assert not self.z.contents_cache
+        assert self.z.package_contents()
+        assert self.z.contents_cache  # Spelling check!
+        self.z.broken_files.add("foo")
+        self.z.contents_cache = "foo"
+        assert self.z.package_contents() != "foo"
+
+    def test_valid_name(self):
+        "Test that the manager can retrieve the correct file name."
+        assert 'install.rdf' in self.z.package_contents()
+
+    def test_read_file(self):
+        """Test that a file can be read from the package."""
+        assert self.z.read('install.rdf') is not None
 
 
-def test_get_list():
-    """Test that the manager can read the file listing."""
-    z = ZipPackage(get_path('xpi/install_rdf_only.xpi'))
-    assert not z.contents_cache
-    assert z.package_contents()
-    assert z.contents_cache  # Spelling check!
-    z.contents_cache = 'foo'
-    eq_(z.package_contents(), 'foo')
+class TestWriteZip(TestCase):
+    def test_write_file(self):
+        """Test that a file can be written in UTF-8 to the package."""
+        with tempfile.NamedTemporaryFile(delete=False) as t:
+            temp_fn = t.name
+            try:
+                z = ZipPackage(temp_fn, mode='w')
+                f, d = 'install.rdf', '注目のコレクション'.decode('utf-8')
+                z.write(f, d)
+                eq_(z.read(f), d.encode('utf-8'))
+            finally:
+                os.unlink(temp_fn)
 
 
-def test_valid_name():
-    "Test that the manager can retrieve the correct file name."
-    z = ZipPackage(get_path('xpi/install_rdf_only.xpi'))
-    contents = z.package_contents()
-    assert 'install.rdf' in contents
+class TestBadZipFile(TestCase):
+    @raises(IOError)
+    def test_missing_file(self):
+        """Tests that the XPI manager correctly reports a missing XPI file."""
+        ZipPackage("foo.bar")
 
-
-def test_read_file():
-    """Test that a file can be read from the package."""
-    z = ZipPackage(get_path('xpi/install_rdf_only.xpi'))
-    assert z.read('install.rdf') is not None
-
-
-def test_write_file():
-    """Test that a file can be written in UTF-8 to the package."""
-    with tempfile.NamedTemporaryFile(delete=False) as t:
-        temp_fn = t.name
+    def test_corrupt_zip(self):
+        """Tests that the XPI manager correctly reports a missing XPI file."""
+        x = ZipPackage(get_path("corrupt.xpi"))
         try:
-            z = ZipPackage(temp_fn, mode='w')
-            f, d = 'install.rdf', '注目のコレクション'.decode('utf-8')
-            z.write(f, d)
-            eq_(z.read(f), d.encode('utf-8'))
-        finally:
-            os.unlink(temp_fn)
+            x.read("install.rdf")
+        except Exception:
+            pass
+        else:
+            raise "Exception should have been raised on corrupt file access."
 
-
-def test_missing_file():
-    """Tests that the XPI manager correctly reports a missing XPI file."""
-    passed = False
-    try:
-        x = ZipPackage('foo.bar')
-    except:
-        passed = True
-    assert passed
+        assert "install.rdf" in x.broken_files
