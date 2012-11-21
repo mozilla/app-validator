267c7be5e6787710494b4573454d97d0fd06ae8b
Matt Basta <me@mattbasta.com>
2012-11-21 12:48:10 -0800

    Error bundle test refactor

--- tests/test_errorbundler.py
+++ tests/test_errorbundler.py
@@ -1,7 +1,7 @@
-import json
 import sys
 from StringIO import StringIO
 
+import simplejson as json
 from mock import patch
 from nose.tools import eq_
 
@@ -12,218 +12,178 @@ from appvalidator.contextgenerator import ContextGenerator
 
 
 class TestErrorBundle(TestCase):
-    pass
 
+    def setUp(self):
+        super(TestErrorBundle, self).setUp()
+        self.setup_err()
 
-def test_message_completeness():
-    """Test we're fully expecting all of the values for a message."""
+    def get_json_results(self):
+        return json.loads(self.err.render_json())
 
-    bundle = ErrorBundle()
+    def test_message_completeness(self):
+        """Test we're fully expecting all of the values for a message."""
 
-    bundle.error(
-        ("id", ),
-        "error",
-        "description",
-        "file",
-        123,  # line
-        456  # column
-    )
+        self.err.error(
+            ("id", ),
+            "error",
+            "description",
+            "file",
+            123,  # line
+            456  # column
+        )
 
-    results = json.loads(bundle.render_json())
-    eq_(len(results["messages"]), 1, "Unexpected number of messages.")
+        results = self.get_json_results()
+        eq_(len(results["messages"]), 1, "Unexpected number of messages.")
 
-    message = results["messages"][0]
-    eq_(message["id"], ["id"])
-    eq_(message["message"], "error")
-    eq_(message["description"], "description")
-    eq_(message["file"], "file")
-    eq_(message["line"], 123)
-    eq_(message["column"], 456)
+        message = results["messages"][0]
+        eq_(message["id"], ["id"])
+        eq_(message["message"], "error")
+        eq_(message["description"], "description")
+        eq_(message["file"], "file")
+        eq_(message["line"], 123)
+        eq_(message["column"], 456)
 
+    def test_json(self):
+        """Test the JSON output capability of the error bundler."""
 
-def test_json():
-    """Test the JSON output capability of the error bundler."""
+        self.err.set_tier(4)
+        self.err.set_tier(3)
 
-    # Use the StringIO as an output buffer.
-    bundle = ErrorBundle() # No color since no output
-    bundle.set_tier(4)
-    bundle.set_tier(3)
+        self.err.error((), "error", "description")
+        self.err.warning((), "warning", "description")
+        self.err.notice((), "notice", "description")
 
-    bundle.error((), "error", "description")
-    bundle.warning((), "warning", "description")
-    bundle.notice((), "notice", "description")
+        results = self.get_json_results()
 
-    results = json.loads(bundle.render_json())
+        eq_(len(results["messages"]), 3)
+        assert not results["success"]
+        eq_(results["ending_tier"], 4)
 
-    eq_(len(results["messages"]), 3)
-    assert not results["success"]
-    eq_(results["ending_tier"], 4)
+    @patch("sys.stdout", StringIO())
+    def test_boring(self):
+        """Test that boring output strips out color sequences."""
 
+        self.err.error((), "<<BLUE>><<GREEN>><<YELLOW>>")
+        self.err.print_summary(no_color=True)
 
-@patch("sys.stdout", StringIO())
-def test_boring():
-    """Test that boring output strips out color sequences."""
+        sys.stdout.seek(0)
+        eq_(sys.stdout.getvalue().count("<<GREEN>>"), 0)
 
-    # Use the StringIO as an output buffer.
-    bundle = ErrorBundle()
-    bundle.error((), "<<BLUE>><<GREEN>><<YELLOW>>")
-    bundle.print_summary(no_color=True)
+    def test_file_structure(self):
+        """
+        Test the means by which file names and line numbers are stored in
+        errors, warnings, and notices.
+        """
 
-    sys.stdout.seek(0)
-    eq_(sys.stdout.getvalue().count("<<GREEN>>"), 0)
+        # Populate the bundle with some test data.
+        self.err.error((), "error", "", "file1", 123)
+        self.err.error((), "error", "", "file2")
 
+        self.err.warning((), "warning", "", "file4", 123)
+        self.err.warning((), "warning", "", "file5")
+        self.err.warning((), "warning")
 
-def test_file_structure():
-    """
-    Test the means by which file names and line numbers are stored in errors,
-    warnings, and messages.
-    """
+        # Load the JSON output as an object.
+        output = self.get_json_results()
 
-    # Use the StringIO as an output buffer.
-    bundle = ErrorBundle()
+        # Do the same for friendly output
+        output2 = self.err.print_summary(verbose=False)
 
-    # Populate the bundle with some test data.
-    bundle.error((), "error", "", "file1", 123)
-    bundle.error((), "error", "", "file2")
+        # Do the same for verbose friendly output
+        output3 = self.err.print_summary(verbose=True)
 
-    bundle.warning((), "warning", "", "file4", 123)
-    bundle.warning((), "warning", "", "file5")
-    bundle.warning((), "warning")
+        # Run some basic tests
+        eq_(len(output["messages"]), 5)
+        assert len(output2) < len(output3)
 
-    # Load the JSON output as an object.
-    output = json.loads(bundle.render_json())
+        messages = ["file1", "file2", "", "file4", "file5"]
 
-    # Do the same for friendly output
-    output2 = bundle.print_summary(verbose=False)
+        for message in output["messages"]:
+            assert message["file"] in messages
+            messages.remove(message["file"])
 
-    # Do the same for verbose friendly output
-    output3 = bundle.print_summary(verbose=True)
+            if isinstance(message["file"], list):
+                pattern = message["file"][:]
+                pattern.pop()
+                pattern.append("")
+                file_merge = " > ".join(pattern)
+                assert output3.count(file_merge)
+            else:
+                assert output3.count(message["file"])
 
-    # Run some basic tests
-    eq_(len(output["messages"]), 5)
-    assert len(output2) < len(output3)
+        assert not messages
 
-    messages = ["file1", "file2", "", "file4", "file5"]
+    def test_notice(self):
+        """Test notice-related functions of the error bundler."""
 
-    for message in output["messages"]:
-        print message
+        self.err.notice((), "")
 
-        assert message["file"] in messages
-        messages.remove(message["file"])
+        # Load the JSON output as an object.
+        output = json.loads(self.err.render_json())
 
-        if isinstance(message["file"], list):
-            pattern = message["file"][:]
-            pattern.pop()
-            pattern.append("")
-            file_merge = " > ".join(pattern)
-            print file_merge
-            assert output3.count(file_merge)
-        else:
-            assert output3.count(message["file"])
+        # Run some basic tests
+        eq_(len(output["messages"]), 1)
 
-    assert not messages
+        assert any(m["type"] == "notice" for m in output["messages"]), (
+            "Notices were not found.")
 
+        assert not self.err.failed()
+        assert not self.err.failed(True)
 
-def test_notice():
-    """Test notice-related functions of the error bundler."""
+    def test_notice_friendly(self):
+        """
+        Test notice-related human-friendly text output functions of the error
+        bundler.
+        """
 
-    # Use the StringIO as an output buffer.
-    bundle = ErrorBundle()
+        self.err.notice((), "foobar")
 
-    bundle.notice((), "")
+        output = self.err.print_summary(verbose=True, no_color=True)
+        assert output.count("foobar")
 
-    # Load the JSON output as an object.
-    output = json.loads(bundle.render_json())
+    def test_initializer(self):
+        """Test that the __init__ paramaters are doing their jobs."""
 
-    # Run some basic tests
-    assert len(output["messages"]) == 1
+        e = ErrorBundle()
+        assert e.determined
+        assert e.get_resource("listed")
 
-    print output
+        e = ErrorBundle(determined=False)
+        assert not e.determined
+        assert e.get_resource("listed")
 
-    has_ = False
+        e = ErrorBundle(listed=False)
+        assert e.determined
+        assert not e.get_resource("listed")
 
-    for message in output["messages"]:
-        print message
+    def test_json_constructs(self):
+        """This tests some of the internal JSON stuff so we don't break zamboni."""
 
-        if message["type"] == "notice":
-            has_ = True
+        self.err.warning((), "Context test",
+                         context=ContextGenerator("x\ny\nz\n"),
+                         line=2, column=0)
+        self.err.notice((), "none")
+        self.err.notice((), "line", line=1)
+        self.err.notice((), "column", column=0)
+        self.err.notice((), "line column", line=1, column=1)
 
-    assert has_
-    assert not bundle.failed()
-    assert not bundle.failed(True)
+        j = self.get_json_results()
 
+        assert "messages" in j
+        assert all(m["context"] == ["x", "y", "z"] for m in j["messages"] if
+                   m["type"] == "warning"), "Warning had wrong context."
 
-def test_notice_friendly():
-    """
-    Test notice-related human-friendly text output functions of the error
-    bundler.
-    """
+        for m in (m for m in j["messages"] if m["type"] == "notice"):
+            if "line" in m["message"]:
+                assert m["line"] is not None
+                assert isinstance(m["line"], int)
+                assert m["line"] > 0
+            else:
+                assert m["line"] is None
 
-    # Use the StringIO as an output buffer.
-    bundle = ErrorBundle()
-
-    bundle.notice((), "foobar")
-
-    # Load the JSON output as an object.
-    output = bundle.print_summary(verbose=True, no_color=True)
-    print output
-
-    assert output.count("foobar")
-
-
-def test_initializer():
-    """Test that the __init__ paramaters are doing their jobs."""
-
-    e = ErrorBundle()
-    assert e.determined
-    assert e.get_resource("listed")
-
-    e = ErrorBundle(determined=False)
-    assert not e.determined
-    assert e.get_resource("listed")
-
-    e = ErrorBundle(listed=False)
-    assert e.determined
-    assert not e.get_resource("listed")
-
-
-def test_json_constructs():
-    """This tests some of the internal JSON stuff so we don't break zamboni."""
-
-    e = ErrorBundle()
-    e.error(("a", "b", "c"), "Test")
-    e.error(("a", "b", "foo"), "Test")
-    e.error(("a", "foo", "c"), "Test")
-    e.error(("a", "foo", "c"), "Test")
-    e.error(("b", "foo", "bar"), "Test")
-    e.warning((), "Context test",
-              context=ContextGenerator("x\ny\nz\n"),
-              line=2, column=0)
-    e.notice((), "none")
-    e.notice((), "line", line=1)
-    e.notice((), "column", column=0)
-    e.notice((), "line column", line=1, column=1)
-
-    results = e.render_json()
-    print results
-    j = json.loads(results)
-
-    assert "messages" in j
-    for m in j["messages"]:
-        if m["type"] == "warning":
-            assert m["context"] == ["x", "y", "z"]
-
-    for m in (m for m in j["messages"] if m["type"] == "notice"):
-        if "line" in m["message"]:
-            assert m["line"] is not None
-            assert isinstance(m["line"], int)
-            assert m["line"] > 0
-        else:
-            assert m["line"] is None
-
-        if "column" in m["message"]:
-            assert m["column"] is not None
-            assert isinstance(m["column"], int)
-            assert m["column"] > -1
-        else:
-            assert m["column"] is None
+            if "column" in m["message"]:
+                assert m["column"] is not None
+                assert isinstance(m["column"], int)
+                assert m["column"] > -1
+            else:
+                assert m["column"] is None
