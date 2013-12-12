ab98581db1916815adec70b00d40957d9af85485
Matt Basta <me@mattbasta.com>
2013-12-12 11:36:43 -0800

    Remove grizwald back-end

--- griz.py
+++
@@ -1,8 +0,0 @@
-import sys
-from appvalidator import validate_app, validate_packaged_app
-
-path = sys.argv[1]
-if path.endswith(".webapp"):
-    print validate_app(path, format="json")
-else:
-    print validate_packaged_app(path, format="json")
